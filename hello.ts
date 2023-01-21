7d9c5904a2392c5572828f45b1c15d7db56b8566
JabezLawrence <jabezcs32@gmail.com>
2023-01-21 11:49:40 +0530

    first commit

---
+++ hello.ts
@@ -0,0 +1,14 @@
+let marks=65
+if (marks>=95 && marks<=100){
+console.log("A+")
+}
+
+else if (marks>=60 && marks<=95){
+    console.log("B+")
+}
+else if (marks>=30 && marks<=59){
+    console.log("c+")
+}
+else{
+    console.log("Sorry")
+}
